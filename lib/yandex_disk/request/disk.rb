70de780d66a1583ce750ed50f1891d634e55b546
yart <jarocki.art@gmail.com>
2024-06-22 21:22:20 +0300

    refactor(request): Disk
    - notation improved

--- lib/yandex_disk/request/disk.rb
+++ lib/yandex_disk/request/disk.rb
@@ -5,7 +5,7 @@ module YandexDisk
     # General information about the user's Yandex Disk: the available space, system folder paths, and so on.
     class Disk < Base
       # @param [Array<String>] fields
-      # @return [Hash] response
+      # @return [YandexDisk::Disk] response
       def request_info(*fields)
         request(:get, path: '/disk', query: { fields: })
       end
