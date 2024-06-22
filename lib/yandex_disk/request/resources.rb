30afc919d46e14130f2a0869f1a080db5a96c735
yart <jarocki.art@gmail.com>
2024-06-22 21:23:55 +0300

    refactor(request): Resources
    - improved notations of all methods

--- lib/yandex_disk/request/resources.rb
+++ lib/yandex_disk/request/resources.rb
@@ -10,7 +10,7 @@ module YandexDisk
       # @option query [Boolean] force_async
       # @option query [String] md5
       # @option query [Boolean] permanently
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def remove(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
 
@@ -25,7 +25,7 @@ module YandexDisk
       # @option query [Boolean] preview_crop
       # @option query [String] preview_size
       # @option query [String] sort
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def request_info(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
 
@@ -36,7 +36,7 @@ module YandexDisk
       # @option query [String] path required
       # @option query [Array<String>] fields
       # @option query [Hash] body required
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def set_custom_properties(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
         raise Request::RequiredKeyError, :body if query[:body].nil?
@@ -49,7 +49,7 @@ module YandexDisk
       # @param [Hash] query
       # @option query [String] path required
       # @option query [Array<String>] fields
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def make_directory(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
 
@@ -62,7 +62,7 @@ module YandexDisk
       # @option query [Array<String>] fields
       # @option query [Boolean] force_async
       # @option query [Boolean] overwrite
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def copy(**query)
         raise Request::RequiredKeyError, :from if query[:from].nil?
         raise Request::RequiredKeyError, :path if query[:path].nil?
@@ -73,7 +73,7 @@ module YandexDisk
       # @param [Hash] query
       # @option query [String] path required
       # @option query [Array<String>] fields
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def get_download_link(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
 
@@ -88,7 +88,7 @@ module YandexDisk
       # @option query [Boolean] preview_crop
       # @option query [String] preview_size
       # @option query [String] sort
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def files_list(**query)
         request(:get, path: "#{path}/files", query:)
       end
@@ -100,7 +100,7 @@ module YandexDisk
       # @option query [Boolean] preview_crop
       # @option query [String] preview_size
       # @option query [String] sort
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def last_uploaded_list(**query)
         request(:get, path: "#{path}/last_uploaded", query:)
       end
@@ -111,7 +111,7 @@ module YandexDisk
       # @option query [Array<String>] fields
       # @option query [Boolean] force_async
       # @option query [Boolean] overwrite
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def move(**query)
         raise Request::RequiredKeyError, :from if query[:from].nil?
         raise Request::RequiredKeyError, :path if query[:path].nil?
@@ -126,7 +126,7 @@ module YandexDisk
       # @option query [Boolean] preview_crop
       # @option query [String] preview_size
       # @option query [String] sort
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def public_files_list(**query)
         request(:get, path: "#{path}/public", query:)
       end
@@ -134,7 +134,7 @@ module YandexDisk
       # @param [Hash] query
       # @option query [String] path required
       # @option query [Array<String>] fields
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def publish(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
 
@@ -144,7 +144,7 @@ module YandexDisk
       # @param [Hash] query
       # @option query [String] path required
       # @option query [Array<String>] fields
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def unpublish(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
 
@@ -155,7 +155,7 @@ module YandexDisk
       # @option query [String] path required
       # @option query [Array<String>] fields
       # @option query [Boolean] overwrite
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def get_upload_link(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
 
@@ -167,7 +167,7 @@ module YandexDisk
       # @option query [String] url required
       # @option query [Boolean] disable_redirects
       # @option query [Array<String>] fields
-      # @return [HTTParty::Response]
+      # @return [YandexDisk::Resources]
       def upload_from_internet(**query)
         raise Request::RequiredKeyError, :path if query[:path].nil?
         raise Request::RequiredKeyError, :url if query[:url].nil?
