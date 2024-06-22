6ac5bb1991a0aea2bb8f174a396f22c04506c764
yart <jarocki.art@gmail.com>
2024-06-22 21:23:08 +0300

    docs(request): Error
    - added notations for classes

--- lib/yandex_disk/request/error.rb
+++ lib/yandex_disk/request/error.rb
@@ -5,14 +5,17 @@ module YandexDisk
     class Error < ::StandardError
     end
 
+    # Wehn a unsupported method sent
     class WrongRequestMethod < Error
       def initialize(method) = super "Method `#{method}' is forbidden. Allowed methods are only #{ALLOWED_METHODS.join(', ')}"
     end
 
+    # When a required key wasn't passed
     class RequiredKeyError < Error
       def initialize(key) = super "Key `#{key}:' is required!"
     end
 
+    # When such a resource is already exists on the Yandex.Disk
     class ResourseAlreadyExists < Error
       def initialize(path) = super "Resourse #{path} already exitst."
     end
